513fa5e4031519f57982631842a4e25ec09d9f3d
Exusiai-applepies <exusiiaii@gmail.com>
2025-05-29 17:24:19 +0200

    MySql

--- src/main/java/com/gachasIberiaAPI/GachasIberiaAPI/GachasIberiaApiApplication.java
+++ src/main/java/com/gachasIberiaAPI/GachasIberiaAPI/GachasIberiaApiApplication.java
@@ -8,14 +8,7 @@ import org.springframework.context.ApplicationContext;
 public class GachasIberiaApiApplication {
 
 	public static void main(String[] args) {
-		try {
-			ApplicationContext context = SpringApplication.run(GachasIberiaApiApplication.class, args);
-			System.out.println("✅ Gachas Iberia API iniciada correctamente");
-		} catch (Throwable t) {
-			t.printStackTrace();
-			// Pausa para que puedas ver el error antes de que el proceso termine
-			try { Thread.sleep(60000); } catch (InterruptedException ignored) {}
-		}
+		ApplicationContext context = SpringApplication.run(GachasIberiaApiApplication.class, args);
 	}
 
 }
